82fcb90c791928009e7dfe1e951e8e63e7d5a759
Lange Siphosethu <128488997+SetthuL@users.noreply.github.com>
2025-02-10 21:19:14 +0200

    updated css

--- script.js
+++ script.js
@@ -15,4 +15,21 @@ function toggleSection(sectionId) {
                 section.classList.remove("hidden");
                 section.classList.add("visible");
             }
+
+            let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+            window.onscroll = function () {
+              if (
+                document.body.scrollTop > 100 ||
+                document.documentElement.scrollTop > 100
+              ) {
+                scrollToTopBtn.style.display = "block";
+              } else {
+                scrollToTopBtn.style.display = "none";
+              }
+            };
+
+            scrollToTopBtn.onclick = function () {
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            };
         }
